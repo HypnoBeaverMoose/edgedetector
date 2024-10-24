93d1d4ec3911dcd7b318e7db491227fd49df37e4
Yasen Atanasov <yasen@rara.co>
2024-10-24 17:20:48 +0300

    find non zero pixels

--- processing.hpp
+++ processing.hpp
@@ -3,9 +3,17 @@
 #include <algorithm>
 #include <iostream>
 
+struct Coord
+{
+    Coord(int x, int y) : X(x), Y(y) {}
+
+    int X;
+    int Y;
+};
+
 int offsets[4][2] = {{0, 1}, {1, 1}, {1, 0}, {1, -1}};
 
-template <typename T> 
+template <typename T>
 T GradientDirection(T left, T right)
 {
     // return std::round((std::atan2((float)left, (float)right) * 57.2958f));
@@ -23,12 +31,30 @@ T GradientDirection(T left, T right)
     return index;
 }
 
-template <typename T> T CombineGradients(T left, T right)
+template <typename T>
+T CombineGradients(T left, T right)
 {
     return std::round(std::sqrt(left * left + right * right));
 }
 
-template <typename T> 
+template <typename T>
+std::vector<Coord> FindNonZeroPixels(const Image<T> &image)
+{
+    std::vector<Coord> result;
+    for (size_t y = 0; y < image.GetHeight(); y++)
+    {
+        for (size_t x = 0; x < image.GetWidth(); x++)
+        {
+            if (image.GetPixel(x, y) > 0)
+            {
+                result.push_back(Coord(x,y));
+            }
+        }
+    }
+    return result;
+}
+
+template <typename T>
 Image<T> FindLocalMaxima(const Image<T> &gradient, const Image<T> &direction)
 {
     if (gradient.GetHeight() != direction.GetHeight() || gradient.GetWidth() != direction.GetWidth())
