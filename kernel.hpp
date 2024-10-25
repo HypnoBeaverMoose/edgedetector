f7350414c22f76a2217ca52e464f584c576a6a16
Yasen Atanasov <yasen@rara.co>
2024-10-25 14:23:41 +0300

    added gaussian kernels

--- kernel.hpp
+++ kernel.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 template <typename T, unsigned int N>
 class Kernel
@@ -13,10 +14,38 @@ public:
 
     int GetSize() { return N; }
 
-private:
+protected:
     std::vector<T> _horizontal;
     std::vector<T> _vertical;
 };
 
-Kernel<float, 3> SobelX((float[3]){1, 0, -1}, (float[3]){1, 2, 1});
-Kernel<float, 3> SobelY((float[3]){1, 2, 1}, (float[3]){1, 0, -1});
+template <unsigned int N>
+class Gaussian : public Kernel<float, N>
+{
+public:
+    Gaussian(float sigma) : Kernel<float, N>((float[N]){}, (float[N]){})
+    {
+        float sigmaSqr = sigma * sigma;
+        float div = 1.0f / std::sqrt(2 * M_PI * sigmaSqr);
+        int size = N;
+        int halfsize = size / 2;
+
+        float sum = 0;
+        for (int x = -halfsize; x <= halfsize; x++)
+        {
+            float val = std::exp(-0.5f * x * x / sigmaSqr) * div;
+            this->_horizontal[x + halfsize] = val;
+            sum += val;
+        }
+        for (size_t i = 0; i < N; i++)
+        {
+            this->_horizontal[i] /= sum;
+        }
+
+        std::copy(this->_horizontal.begin(), this->_horizontal.end(), this->_vertical.begin());
+    }
+};
+
+Kernel<float, 3> SobelX((float[3]){0.5f, 0, -0.5f}, (float[3]){0.25f, 0.5f, 0.25f});
+Kernel<float, 3> SobelY((float[3]){0.25f, 0.5f, 0.25f}, (float[3]){0.5f, 0, -0.5f});
+Gaussian<5> Gaussian5(2);
--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@
 int main(int argc, char **argv)
 {
     Image<float> im = TgaImageFactory("input.tga").GetImage();
-
-    Image<float> resultX = im.GetConvolvedSeparable(SobelX.GetHorizontal(), SobelX.GetGetVertical());
-    Image<float> resultY = im.GetConvolvedSeparable(SobelY.GetHorizontal(), SobelY.GetGetVertical());
+    Image<float> blurred = im.GetConvolvedSeparable(Gaussian5.GetHorizontal(), Gaussian5.GetGetVertical());
+    Image<float> resultX = blurred.GetConvolvedSeparable(SobelX.GetHorizontal(), SobelX.GetGetVertical());
+    Image<float> resultY = blurred.GetConvolvedSeparable(SobelY.GetHorizontal(), SobelY.GetGetVertical());
 
     Image<float> gradient = Image<float>::CombineImages(resultX, resultY, CombineGradients<float>);
     Image<float> direction = Image<float>::CombineImages(resultX, resultY, GradientDirection<float>);
     Image<float> result = FindLocalMaxima(gradient, direction);
-    result.ApplyThreshold(1, 0.12f);
 
-    // gradient.Normalize(1.0f);
+
+    // // gradient.Normalize(1.0f);
     FileUtils::SaveImage(result, "output.tga");
 }
