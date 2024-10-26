1e436cfcc841d2e46f0e9abd09fcbb52f1f6a372
Yasen Atanasov <yasen@rara.co>
2024-10-26 15:50:56 +0300

    minor hot path optimizations

--- edge-detector.cpp
+++ edge-detector.cpp
@@ -5,9 +5,11 @@
 #include <algorithm>
 #include <iostream>
 
-int offsets[4][2] = {{0, 1}, {1, 1}, {1, 0}, {1, -1}};
+int offsets[8] = {0, 1, 1, 1, 1, 0, 1, -1};
+int connectivity[16] = {-1, -1, 0, -1, 1, -1, -1, 0, 1, 0, -1, 1, 0, 1, 1, 1};
 
-Kernel<float, 3> SobelX((float[3]){1, 0, -1}, (float[3]){1, 2, 1});
+Kernel<float, 3>
+    SobelX((float[3]){1, 0, -1}, (float[3]){1, 2, 1});
 Kernel<float, 3> SobelY((float[3]){1, 2, 1}, (float[3]){1, 0, -1});
 
 template <typename TRet, typename TLeft, typename TRight>
@@ -82,8 +84,8 @@ void EdgeDetector<T>::FindInitialEdges(const Image<T> &filterX, const Image<T> &
     Image<T> gradient(filterX.GetWidth(), filterY.GetHeight());
     Image<unsigned char> direction(filterX.GetWidth(), filterY.GetHeight());
 
-    CombineImages<T, T, T>(filterX, filterY, gradient, CombineGradients<T,T,T>);
-    CombineImages<unsigned char, T, T>(filterX, filterY, direction, FindGradientDirection<unsigned char,T,T>);
+    CombineImages<T, T, T>(filterX, filterY, gradient, CombineGradients<T, T, T>);
+    CombineImages<unsigned char, T, T>(filterX, filterY, direction, FindGradientDirection<unsigned char, T, T>);
 
     if (_debug)
     {
@@ -124,11 +126,11 @@ void EdgeDetector<T>::FindLocalMaxima(const Image<T> &gradient, const Image<unsi
         for (size_t x = 0; x < width; x++)
         {
             unsigned char directionIndex = direction.GetPixelUnsafe(x, y);
-            int positiveX = x + offsets[directionIndex][0];
-            int positiveY = y + offsets[directionIndex][1];
+            int positiveX = x + offsets[directionIndex * 2];
+            int positiveY = y + offsets[directionIndex * 2 + 1];
 
-            int negativeX = x - offsets[directionIndex][0];
-            int negativeY = y - offsets[directionIndex][1];
+            int negativeX = x - offsets[directionIndex * 2];
+            int negativeY = y - offsets[directionIndex * 2 + 1];
 
             T before = gradient.GetPixel(positiveX, positiveY);
             T after = gradient.GetPixel(negativeX, negativeY);
@@ -165,26 +167,20 @@ void EdgeDetector<T>::TrackEdges(const Image<T> &input, Image<T> &result, T edge
 template <typename T>
 T EdgeDetector<T>::DetermineEdgeValue(const Image<T> &input, int x, int y, T edgeValue) const
 {
-    T val = input.GetPixel(x, y, Image<T>::CLAMP);
+    T val = input.GetPixelUnsafe(x, y);
     if (!(val > 0 && val < edgeValue))
     {
         return val;
     }
 
-    for (int offset_y = -1; offset_y <= 1; offset_y++)
+    for (size_t i = 0; i < 8; i++)
     {
-        for (int offset_x = -1; offset_x <= 1; offset_x++)
+        int realX = x + connectivity[i * 2];
+        int realY = y + connectivity[i * 2 + 1];
+        T pixel = input.GetPixel(realX, realY, Image<T>::CLAMP);
+        if (std::abs(pixel - edgeValue) < std::numeric_limits<T>::epsilon())
         {
-            int realX = x + offset_x;
-            int realY = y + offset_y;
-            if (offset_x != 0 && offset_y != 0)
-            {
-                T pixel = input.GetPixel(realX, realY, Image<T>::CLAMP);
-                if (std::abs(pixel - edgeValue) < std::numeric_limits<T>::epsilon())
-                {
-                    return edgeValue;
-                }
-            }
+            return edgeValue;
         }
     }
 
